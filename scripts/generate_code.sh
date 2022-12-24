9e1a1adb4b2dffb98972862c7593154fde0d053a
Yuki Bobier <engineer.yuki.bobier@gmail.com>
2022-12-24 04:23:05 +0000

    build: add generate_code.sh

---
+++ scripts/generate_code.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+generate_code() {
+    IN=$1
+    OUT=$2
+    PACKAGE=$3
+
+    protoc \
+    --go_out=${OUT} \
+    --go_opt=paths=import \
+    --go_opt=M${IN}=./${PACKAGE} \
+    --go-grpc_out=${OUT} \
+    --go-grpc_opt=paths=import \
+    --go-grpc_opt=M${IN}=./${PACKAGE} \
+    ${IN}
+}
+
+generate_code proto/chat.proto . chat
